34c41fc5e2ee48ac198cac8dc1b03554a354e6ed
kamkara <lnclass.kamkara@gmail.com>
2021-03-12 07:10:11 +0000

    add phone contact on Index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_11_235811) do
+ActiveRecord::Schema.define(version: 2021_03_12_070811) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2021_03_11_235811) do
     t.string "role", default: "student"
     t.string "slug"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["phone_contact"], name: "index_users_on_phone_contact", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
