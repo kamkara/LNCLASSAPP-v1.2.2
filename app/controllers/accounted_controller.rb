f94e96639e0f566f7d23b86833df7a42053cdf09
kamkara <lnclass.kamkara@gmail.com>
2021-03-12 09:01:09 +0000

    add accounted params

--- app/controllers/accounted_controller.rb
+++ app/controllers/accounted_controller.rb
@@ -1,10 +1,34 @@
 class AccountedController < ApplicationController
+  before_action :find_materials
+  before_action :find_levels
+  before_action :find_citytowns
+  before_action :find_schools
+  
   def student
   end
 
   def teacher
   end
-
+  
   def team
   end
+  private
+  
+  #enable material
+    def find_materials
+      @materials = Material.all
+    end
+  
+    #enable level
+    def find_levels
+      @levels = Level.all
+    end
+
+    def find_citytowns
+      @citytowns = Citytown.all
+    end
+
+    def find_schools
+      @schools = School.all
+    end
 end
