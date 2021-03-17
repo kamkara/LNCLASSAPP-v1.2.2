95f170a36680aa61083eaad89b736594c7a8774b
kamkara <lnclass.kamkara@gmail.com>
2021-03-17 23:13:11 +0000

    teacher admin space

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,6 +18,8 @@ require("channels")
  const images = require.context('../images', true)
  const imagePath = (name) => images(name, true)
 import "../css/application.css"
+import "../charts/chart.js"
+
 require("trix")
 require("@rails/actiontext")
 
