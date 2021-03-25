d4b30b1ab4c90fed1c8d3a56f44a5bf884d73560
kamkara <lnclass.kamkara@gmail.com>
2021-03-25 23:41:09 +0000

    add asset precompile

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,14 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
+
+
+const { environment } = require('@rails/webpacker')
+const erb = require('./loaders/erb')
+const customConfig = require('./custom')
+
+environment.config.set('output.filename', '[name].js')
+environment.config.merge(customConfig)
+environment.config.merge({ devtool: 'none' })
+environment.loaders.prepend('erb', erb)
+module.exports = environment
